c3b79547300b0c31dc2ea336ab31d0e3eab111b2
Kintuda <32006074+Kintuda@users.noreply.github.com>
2017-11-21 01:09:52 -0200

    3.0

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var session = require('cookie-session')
+var compression = require('compression')
 
 var app = express();
 
 var expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'vash');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression())
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
